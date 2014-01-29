32e468455dd6db5c8884d68d8bdb873ccac3f205
lealtag <development@lealtag.com>
2014-01-29 11:02:43 -0430

    se quito verificacion de si la fecha de creacion es nula

--- agregar_createdAt_logUserLocals.js
+++ agregar_createdAt_logUserLocals.js
@@ -2,14 +2,12 @@ db = db.getSiblingDB('api_production')
 cursor = db.log_user_locals.find();
 while ( cursor.hasNext() ) {
    logClient = cursor.next();
-   if(logClient.created_at == null){
    print("\t "+logClient.client)
    cursor2 = db.invoices.aggregate([{$match : {_id:{$in : logClient.invoices}}},{ $group: { _id:0, minDate: { $min: "$date"} } }])
    minD = cursor2.result[0].minDate  
    logClient.created_at = minD
    printjson(logClient)
    db.log_user_locals.save(logClient)
-   }
 	
 }
 	
